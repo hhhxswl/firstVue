b13dadda8ce3ae31c731f16a86e7b2ecb818a4f9
hhhxswl <daishijie24@qq.com>
2020-02-12 19:40:38 +0800

    修改图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { Header } from 'mint-ui';
 import 'mint-ui/lib/style.css';  //如果没有出现样式，则需要全局引用一下样式
 import app from './App.vue';
 import './lib/mui/css/mui.css';
+import './lib/mui/css/icons-extra.css';  //扩展图标和下面的扩展字体要导入
+import './lib/mui/fonts/mui-icons-extra.ttf'
 Vue.component(Header.name, Header);
 
   var vm = new Vue({
